ec2381f4e734997a776b997ce142321bd752c556
jhosuappp <joshua@150porciento.com>
2022-07-25 16:11:54 -0500

    se termina de actualizar los toggle

--- sources/js/mainscript.js
+++ sources/js/mainscript.js
@@ -1,26 +1,33 @@
-var getAllToggles = document.querySelectorAll('.toggle__top');
-
+var getAllToggles = document.querySelectorAll('.toggle__contenedor .toggle__top');
+var getAllTogglesBottom = document.querySelectorAll('.toggle__contenedor .toggle__bottom');
 
 getAllToggles.forEach((data, indice) =>{
 
+    var getNextElementSibling = data.nextElementSibling;
+
     if(indice == 0){
         data.classList.add('active');
-        data.nextElementSibling.style.height = data.nextElementSibling.scrollHeight + 20 + 'px';
+        getNextElementSibling.style.height = data.nextElementSibling.scrollHeight + 20 + 'px';
     }
 
     data.addEventListener('click', ()=>{
 
-        let getNextElementSibling = data.nextElementSibling;
         let scrollHeigthElement = getNextElementSibling.scrollHeight;
 
+        getAllToggles.forEach((data)=>{
+            data.classList.remove('active');
+        });
+        getAllTogglesBottom.forEach((data)=>{
+            data.style.height = '0px';
+        });
 
-        data.classList.toggle('active');
+        data.classList.add('active');
         if(data.classList.contains('active')){
-            getNextElementSibling.style.height = scrollHeigthElement + 20 + 'px';
+            getNextElementSibling.style.height = scrollHeigthElement + 'px';
         }else{
             getNextElementSibling.style.height = '0px';
         }
-
+        
     });
-    
+
 });
